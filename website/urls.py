eeb196892b974aafe7a5024422873843c7ba7a02
Nate Brown <nathan.a.brown@outlook.com>
2021-07-09 17:28:06 -0400

    create base.html and add static tags to all templates

--- website/urls.py
+++ website/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('service', views.service, name='service'),
+    path('pricing', views.pricing, name='pricing'),
+    path('contact', views.contact, name='contact'),
+    path('blog', views.blog, name='blog'),
+    path('blog-details', views.blog_details, name='blog_details'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,4 +1,28 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'index.html', {})
+    return render(request, 'pages/index.html', {})
+
+
+def about(request):
+    return render(request, 'pages/about.html', {})
+
+
+def service(request):
+    return render(request, 'pages/service.html', {})
+
+
+def pricing(request):
+    return render(request, 'pages/pricing.html', {})
+
+
+def contact(request):
+    return render(request, 'pages/contact.html', {})
+
+
+def blog(request):
+    return render(request, 'pages/blog.html', {})
+
+
+def blog_details(request):
+    return render(request, 'pages/blog-details.html', {})
